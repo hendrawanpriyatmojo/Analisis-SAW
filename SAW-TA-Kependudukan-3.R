cb54b54d0607decc82f84e15709496320e3aaf93
hendrawan <44942638+hendrawanpriyatmojo@users.noreply.github.com>
2018-11-11 19:53:57 +0700

    Add files via upload

---
+++ SAW-TA-Kependudukan-3.R
@@ -0,0 +1,8 @@
+Data <- read.csv(file.choose(), header=TRUE)
+DESA.ID<- (Data$Desa.ID)
+NILAI.X<- (Data$Nilai.X)
+NILAI.Y<- (Data$Nilai.Y)
+data <- data.frame(DESA.ID, NILAI.X, NILAI.Y)
+plot(NILAI.X,type = "o", col = "red", xlab = "DESA.ID", ylab = "NILAI",
+     main = "Kependudukan")
+lines(NILAI.Y, type = "o", col = "blue")
